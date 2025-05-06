fd08b6ba85bf6641b15f9c06c6f7f6547464ff59
jenarcotic <106402903+jenarcotic@users.noreply.github.com>
2025-05-06 13:28:30 +0000

    atualizacao

--- aula 1/x01.py
+++ aula 1/x01.py
@@ -1,1 +1,19 @@
-print("Olá")
+class ingresso:
+    def __init__(self):
+        self.dia = "dom"
+        self.hora = 18
+    def entrada_inteira(self):
+        if self.dia == "dom": return 20
+        return 15
+    
+x = ingresso()
+print(x.dia, x.hora)
+print(x.entrada_inteira())
+
+
+class cinema:
+    def __init__(self):     
+        self.dia
+        self.hora
+    def entrada_inteira(self):
+        if sel.dia == "dom"
